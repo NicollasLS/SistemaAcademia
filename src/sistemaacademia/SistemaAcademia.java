e66b76decbdb26bd60b6455d0327341b27770bb2
Nicollas LS <nicollas.lima13@gmail.com>
2025-04-17 21:23:42 -0300

    ok

--- src/sistemaacademia/SistemaAcademia.java
+++ src/sistemaacademia/SistemaAcademia.java
@@ -4,6 +4,8 @@
  */
 package sistemaacademia;
 
+import java.util.ArrayList;
+import java.util.Scanner;
 import model.AlunoAcademia;
 
 /**
@@ -17,16 +19,70 @@ public class SistemaAcademia {
      */
     public static void main(String[] args) {
         
+        Scanner scanner = new Scanner (System.in);
+        ArrayList<AlunoAcademia> alunos = new ArrayList<> ();
+        int opcao;
+            
+        do {
+            System.out.println("\n====Menu Academia====");
+            System.out.println("1. Cadastrar Aluno ");
+            System.out.println("2. Listar Alunos e IMC ");
+            System.out.println("3. Sair ");
+            System.out.println("4. Escolha um opção: ");
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+            
+            
+            switch (opcao) {
+            case 1:
+                  
+            
+            System.out.println("Nome: ");
+            String nome = scanner.nextLine();
+            //System.out.println("Idade: ");
+            //int idade = scanner.nextInt();
+            System.out.println("Peso (Kg): ");
+            double peso = scanner.nextDouble();
+            System.out.println("Altura (m): ");
+            double altura = scanner.nextDouble();
+            alunos.add(new AlunoAcademia(nome, peso, altura));
+            break;
+            
+            case 2:
+            
+            if (alunos.isEmpty()) {
+            System.out.println("Nenhum aluno cadastrado");
+        } else {
+        System.out.println("\n --- Lista de alunos --- ");
+        for (AlunoAcademia aluno : alunos) {
+        System.out.println(aluno);
+              }
+           }
+        break;
         
-        AlunoAcademia aluno1 = new AlunoAcademia ("Pedro", 85.5, 1.75);   
-         AlunoAcademia aluno2 = new AlunoAcademia ("Ana", 60, 1.56);
-         AlunoAcademia aluno3 = new AlunoAcademia ("Jussara", 88, 1.70);
-         
-         aluno1.exibirStatus ();
-         aluno2.exibirStatus();
-         aluno3.exibirStatus();
+        case 3:
+        System.out.println("Encerrando o sistema... ");
+        break;
+        
+        default:
+        System.out.println("Opcao invalida. ");
+              }
+            }while (opcao !=3);
+        scanner.close();
         
     }
+}
+        
+        
+        //AlunoAcademia aluno1 = new AlunoAcademia ("Pedro", 85.5, 1.75);   
+         //AlunoAcademia aluno2 = new AlunoAcademia ("Ana", 60, 1.56);
+         //AlunoAcademia aluno3 = new AlunoAcademia ("Jussara", 88, 1.70);
+         
+         //aluno1.exibirStatus ();
+         //aluno2.exibirStatus();
+         //aluno3.exibirStatus();
+        
+    
     
     
 }
